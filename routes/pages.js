fbbe16711bee5b49441f0453e5b1252ca1666f78
Maigh Kaur Jammu <60054898+mjk7817@users.noreply.github.com>
2021-04-12 14:53:56 -0600

    Update pages.js

--- routes/pages.js
+++ routes/pages.js
@@ -1,15 +1,28 @@
 const express = require("express");
-const authController = require('../controllers/auth');
-const router = express.Router();
+const app = express();
 const mysql = require("mysql");
+//const serv = require('http').createServer(app);
+const dotenv = require("dotenv");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const port = process.env.PORT || 3000;
+var pgp = require('pg-promise')();
 
-const db = mysql.createConnection({
+app.listen(port, () => {
+  console.log('Server successfully started at port %d', port);
+});
+
+ let dbConfig = { //these need to be our local configurations
     host: process.env.DATABASE_HOST,
     user: process.env.DATABASE_USER,
     password: process.env.DATABASE_PASSWORD,
-    database: process.env.DATABASE
-})
-
+    database: process.env.DATABASE,
+    multipleStatements: true
+};
+  
+const isProduction = process.env.NODE_ENV === 'production';
+dbConfig = isProduction ? process.env.DATABASE_URL : dbConfig;
+var db = pgp(dbConfig);
 
 
 router.get('/', (req, res) => {
