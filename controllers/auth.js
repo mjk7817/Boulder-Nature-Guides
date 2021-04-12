09ab004787b36223c5d96c8d5efe5683a24844af
Maigh Kaur Jammu <60054898+mjk7817@users.noreply.github.com>
2021-04-12 12:50:12 -0600

    Update auth.js

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,4 +1,4 @@
-const mysql = require("mysql");
+//const mysql = require("mysql");
 const jwt = require("jsonwebtoken");
 const bcrypt = require("bcryptjs");
 const email_regex = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
